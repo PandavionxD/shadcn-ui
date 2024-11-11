87c4d4dab35463c80ee457dd804f43cfd411b6dc
Alex Daniel <1151372@senati.pe>
2024-11-10 20:35:32 -0500

    Terminando alert component

--- src/app/dashboard/alert/page.tsx
+++ src/app/dashboard/alert/page.tsx
@@ -1,8 +1,36 @@
+import { Title } from "@/components/Title";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal, CircleAlert, CircleX } from "lucide-react";
 
 export default function AlertPage() {
   return (
-    <div>
-      <h1>AlertPage</h1>
+    <div className="grid gap-6">
+      <Title titulo="Alertas" />
+
+      <Alert>
+        <Terminal className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant={"destructive"}>
+        <CircleX className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
+      <Alert variant={"success"}>
+        <CircleAlert className="h-4 w-4" />
+        <AlertTitle>Heads up!</AlertTitle>
+        <AlertDescription>
+          You can add components to your app using the cli.
+        </AlertDescription>
+      </Alert>
+
     </div>
   );
 }
