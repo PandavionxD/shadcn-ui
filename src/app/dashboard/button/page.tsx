a7f424f38668af9fce34e00cd350188813a34e2a
Alex Daniel <1151372@senati.pe>
2024-11-10 21:07:53 -0500

    Button component

---
+++ src/app/dashboard/button/page.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { ChevronRight, Loader2, Mail } from "lucide-react";
+
+export default function buttonPage() {
+  return (
+    <div className="flex gap-4 flex-wrap ">
+      <Button variant={"default"}>default </Button>
+      <Button disabled variant={"default"}>disabled </Button>
+      <Button variant={"destructive"}>destructive </Button>
+      <Button variant={"ghost"}>ghost </Button>
+      <Button variant={"link"}>link </Button>
+      <Button variant={"outline"}>outline </Button>
+      <Button variant={"secondary"}>secondary </Button>
+      <Button variant={"success"}>success </Button>
+      <Button capitalize={false} >capitalize false </Button>
+      <Button variant="outline" size="icon">
+      <ChevronRight />
+    </Button>
+    <Button>
+      <Mail /> Login with Email
+    </Button>
+    <Button disabled>
+      <Loader2 className="animate-spin" />
+      Please wait
+    </Button>
+    </div>
+  );
+}
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+  "inline-flex items-center  justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
     variants: {
       variant: {
@@ -19,6 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        success: "text-white bg-emerald-500 shadow hover:bg-emerald-600 border ",
       },
       size: {
         default: "h-9 px-4 py-2",
@@ -37,15 +38,16 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  asChild?: boolean;
+  capitalize ?:boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, capitalize = true, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ variant, size, className }),{capitalize})}
         ref={ref}
         {...props}
       />
